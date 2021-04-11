f6aab6d60d7e883c6f7d25c28e2dad63ef73ac0c
Ethan McCue <ethan.mccue@lumanu.com>
2021-04-11 18:54:31 -0400

    have it be tried

--- src/main/java/growtopia/EventHandlerImpl.java
+++ src/main/java/growtopia/EventHandlerImpl.java
@@ -2,12 +2,21 @@ package growtopia;
 
 import growtopia.enet.Enet;
 import growtopia.enet.EventHandler;
+import java.nio.charset.StandardCharsets;
+import java.util.EnumSet;
 
 public final class EventHandlerImpl implements EventHandler {
     @Override
     public void onReceive(Enet.Event.Receive receiveEvent) {
         System.out.println(receiveEvent);
         System.out.println(receiveEvent.dataAsString());
+
+        receiveEvent.peer().send(
+                Enet.Packet.create(
+                        "hello".getBytes(StandardCharsets.US_ASCII),
+                        EnumSet.of(Enet.Packet.Flag.RELIABLE)
+                )
+        );
     }
 
     @Override
